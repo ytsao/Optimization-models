a31c400d449e6dd6a765e58acea90b86a8967cf5
YiNungTsao <t104370040@ntut.org.tw>
2023-11-04 21:44:05 +0100

    update

---
+++ Code/Julia/my_Edmonds_Karp.jl
@@ -0,0 +1,33 @@
+function main()
+
+  num_nodes = 6
+  
+  capacity_matrix = [
+                     0 15 12 0 0 0;
+                     0 0 0 10 0 0 ;
+                     0 10 0 5 0 0 ;
+                     0 0 0 0 0 8 8;
+                     0 0 0 6 0 0 13;
+                     0 0 0 0 0 0 0 ;
+                     ]
+  residual_matrix = [
+                     0 0 0 0 0 0;
+                     0 0 0 0 0 0;
+                     0 0 0 0 0 0;
+                     0 0 0 0 0 0;
+                     0 0 0 0 0 0;
+                     0 0 0 0 0 0;
+                     ]
+
+
+  
+
+end
+
+function bfs()
+
+end
+
+
+main()
+
